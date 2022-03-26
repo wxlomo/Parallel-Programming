3996086eb760d3c3d2c679deaee6c4437c764b54
Wèixuān <weixuan.yang@mail.utoronto.ca>
2022-03-26 16:55:13 -0400

    Update FetchAndAdd.c

--- TestCodes/FetchAndAdd.c
+++ TestCodes/FetchAndAdd.c
@@ -3,6 +3,7 @@
   Creator : Weixuan Yang
   Used to test the atomic functions fetch_and_add and fetch_and_sub.
 */
+
 #include <stdio.h>
 
 void main(void){
@@ -15,6 +16,12 @@ void main(void){
 	printf("%d\n", t);
 	printf("%d - 2 = ", __sync_fetch_and_sub(&t, 2));
 	printf("%d\n", t);
-	printf("%d - 1 = ", __sync_fetch_and_sub(&t, 1));
+	printf("%d - 2 = ", __sync_fetch_and_sub(&t, 2));
+	printf("%d\n", t);
+	printf("%d + 1 = ", __sync_fetch_and_add(&t, 1));
+	printf("%d\n", t);
+	printf("%d - 2 = ", __sync_fetch_and_sub(&t, 2));
+	printf("%d\n", t);
+	printf("%d + 3 = ", __sync_fetch_and_add(&t, 3));
 	printf("%d\n", t);
 }
